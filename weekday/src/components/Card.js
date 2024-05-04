5cbe5a96a0bcb071450d6ffb93bea2bfbc3407b2
Himanshu Shukla <himanshu2361999@gmail.com>
2024-05-04 16:01:56 +0530

    feat : Card Raw HTML

---
+++ weekday/src/components/Card.js
@@ -0,0 +1,30 @@
+const Card = () => {
+    return <div>
+        <div> <p>Posted 10 days ago </p></div>
+        <div>
+            <div> Company image </div>
+            <div>
+                <p> Google </p>
+                <p> Software Engineer </p>
+            </div>
+        </div>
+        <div>
+            <p> Estimated Salary : 18 - 35 LPA </p>
+        </div>
+        <div>
+            <h3> About Company </h3>
+            <h4> About Us </h4>
+            <p> Lorem ipsum is derived from the Latin “dolorem ipsum” roughly translated as “pain itself.” Lorem ipsum presents the sample font and orientation of writing on web pages and other software applications where content is not the main concern of the developer  </p>
+            <button> View job </button>
+        </div>
+        <div>
+            <p> Minimum experience </p>
+            <p> 8 years </p>
+        </div>
+        <div>
+            <button> Easy apply</button>
+            <button> Unlock referral asks</button>
+        </div>
+    </div>
+}
+export default Card
